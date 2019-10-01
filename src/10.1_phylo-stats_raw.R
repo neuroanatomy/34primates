07e5d23fde9b34dd10ffae149cb14ae0fbccf931
Roberto Toro <roberto.toro@gmail.com>
2019-10-01 17:28:32 +0200

    Update 10.1_phylo-stats_raw.R

--- src/10.1_phylo-stats_raw.R
+++ src/10.1_phylo-stats_raw.R
@@ -4,8 +4,8 @@
 #
 
 # Install required packages
-install.packages("ape") #,repos = "http://cran.us.r-project.org")
-install.packages("phytools") #,repos = "http://cran.us.r-project.org")
+install.packages("ape",repos = "http://cran.us.r-project.org")
+# install.packages("phytools") #,repos = "http://cran.us.r-project.org")
 install.packages("corrplot",repos = "http://cran.us.r-project.org")
 
 # Notes
