269d3d6a792be1508eedb16d317788d44b77256d
Roberto Toro <roberto.toro@gmail.com>
2019-10-01 17:49:54 +0200

    Create 10.0_install.R

---
+++ src/10.0_install.R
@@ -0,0 +1,4 @@
+# Install required packages
+install.packages("ape", repos="https://cloud.r-project.org", quiet=TRUE)
+install.packages("phytools", repos="https://cloud.r-project.org", quiet=TRUE)
+install.packages("corrplot", repos="https://cloud.r-project.org", quiet=TRUE)
