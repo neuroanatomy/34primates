9cb1705e550448baa0cb3fc0cbde114d02ec4a94
Roberto Toro <roberto.toro@gmail.com>
2019-10-04 18:06:57 +0200

    add R devtools package

--- src/10.0_install.R
+++ src/10.0_install.R
@@ -1,8 +1,8 @@
 # Install required packages
-#install.packages("ape", repos="https://cloud.r-project.org", quiet=TRUE)
-#install.packages("phytools", repos="https://cloud.r-project.org", quiet=TRUE)
 install.packages("corrplot", repos="https://cloud.r-project.org", quiet=TRUE)
+install.packages("devtools", repos="https://cloud.r-project.org", quiet=TRUE)
 
 library("ape")
 library("phytools")
 library("corrplot")
+library("devtools")
